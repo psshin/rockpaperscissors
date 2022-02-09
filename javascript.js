7e6e0723bbae1a5a2c0abef91f8f11695adac881
psshin <peter.s.shin@gmail.com>
2022-02-08 16:59:59 -0800

    Add working 5-round game to javascript.js

--- javascript.js
+++ javascript.js
@@ -1,4 +1,9 @@
-// Returns computer's random hand
+// Rock, Paper, Scissors!
+
+let roundsPlayed = 0;
+let computerScore = 0;
+let playerScore = 0;
+let drawRound = 0;
 
 function computerPlay() {
     const handOptions = ["Rock", "Paper", "Scissors"]
@@ -12,28 +17,45 @@ function myPlay() {
     return myHand;
 }
 
-//function playRound(playerSelection, computerSelection) {
-//    let win = false;
-//    return win ? "Congrats! You win!" : ` You lose! ${computerSelection} beats ${playerSelection}! `;
-//}
-
-// Play
-//const playerSelection = myPlay();
-//const computerSelection = computerPlay();
-//console.log(playRound(playerSelection, computerSelection));
-//}
+function playRound(playerSelection, computerSelection) {
+    let win = false;
+    if (playerSelection === computerSelection) {
+        roundsPlayed--;
+        drawRound++;
+        return "Draw!";
+    }
+    if (
+        (playerSelection === "Rock" && computerSelection === "Scissors") ||
+        (playerSelection === "Paper" && computerSelection === "Rock") ||
+        (playerSelection === "Scissors" && computerSelection === "Paper")
+    ) {
+        win = true;
+        playerScore++;
+    }
+    if (
+        (computerSelection === "Rock" && playerSelection === "Scissors") ||
+        (computerSelection === "Paper" && playerSelection === "Rock") ||
+        (computerSelection === "Scissors" && playerSelection === "Paper")
+    ) {
+        win = false;
+        computerScore++;
+    }
+    return win ? "Congrats! You win!" : ` You lose! ${computerSelection} beats ${playerSelection}! `;
+}
 
 //Game
+
 function game() {
-    let roundsPlayed = 0;
-    let computerScore = 0;
-    let playerScore = 0;
-    for (var i = 0; i <=5; i++) {
+    for (var i = 0; i >= 0; i++) {
         roundsPlayed++;
-        if (roundsPlayed > 5) {
-            return "Game Over.";
+        const playerSelection = myPlay();
+        const computerSelection = computerPlay();
+        console.log(playRound(playerSelection, computerSelection));
+        if (roundsPlayed === 5) {
+            return console.log(` Game Over! Computer Score: ${computerScore} | Your Score: ${playerScore} | Draw: ${drawRound} `);
         } else {
-            console.log(roundsPlayed);
+            console.log(` ${roundsPlayed} of 5 Rounds Played `);
         }
     }
 }
+game();
